33bb588ea4e800ec2961be0c9cac210642d0d3f2
Peter Graham <peterghm@gmail.com>
2011-12-10 12:24:21 -0500

    Initial Commit

---
+++ cocoa-example/PGGDocument.h
@@ -0,0 +1,13 @@
+//
+//  PGGDocument.h
+//  cocoa-example
+//
+//  Created by Peter Graham on 12/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PGGDocument : NSDocument
+
+@end
---
+++ cocoa-example/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  cocoa-example
+//
+//  Created by Peter Graham on 12/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
